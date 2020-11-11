337c9798128c1422d63639c5559005eae157359c
hazim0001 <hazim_alsayed@hotmail.com>
2020-11-11 16:50:58 -0600

    style_added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+puts "creating random flats"
+10.times do
+  Flat.create!(
+    name: Faker::Company.catch_phrase,
+    address: Faker::Address.secondary_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: (Faker::Number.decimal_part(digits: 4)).to_i,
+    number_of_guests: (Faker::Number.decimal_part(digits: 2)).to_i
+  )
+end
+puts "done"
